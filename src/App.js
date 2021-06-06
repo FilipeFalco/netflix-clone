8427c8e2bc2aadfc877837da80a6039abb7d1b18
Filipe <falcoefilipe@hotmail.com>
2021-06-06 12:46:59 -0400

    Adicionado FeaturedMovie, Header e MovieRow na pagina

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
-import FeatureMovie from './components/FeatureMovie';
+import FeatureMovie from './components/FeaturedMovie';
+import Header from './components/Header'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -26,8 +28,23 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {window.removeEventListener('scroll', scrollListener);}
+  })
+
   return (
     <div className="page">
+
+      <Header black={blackHeader}/>
 
       {featureData && 
         <FeatureMovie item={featureData} />
@@ -38,6 +55,18 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por Filipe Falco<br />
+        Direitos de imagem para Netflix<br />
+        Dados fornecidos por themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" />
+        </div>
+      }
     </div>
   );
 }
