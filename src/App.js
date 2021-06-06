9847be0d35921ade0aea40d46703bf44dc427883
Filipe <falcoefilipe@hotmail.com>
2021-06-05 20:19:30 -0400

    Criado o banner superior com filme em destaque

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o featured
+      let toprated = list.filter(i => i.slug === 'toprated');
+      let randomChosen = Math.floor(Math.random() * (toprated[0].items.results.length - 1));
+      let chosen = toprated[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+
+      {featureData && 
+        <FeatureMovie item={featureData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+    </div>
+  );
+}
