4c8a2204c7a1438f3780be39fe419eebd2d86cf6
root <root@DESKTOP-1D14QK7.localdomain>
2021-06-06 15:28:41 -0400

    Revert "Adicionado a featured para os originais netflix"
    This reverts commit 103b68ef236cfd34d5a6ff1f52ab16130397d9e9.

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ export default () => {
       setMovieList(list);
 
       // Pegando o featured
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-      let chosen = originals[0].items.results[randomChosen];
+      let toprated = list.filter(i => i.slug === 'toprated');
+      let randomChosen = Math.floor(Math.random() * (toprated[0].items.results.length - 1));
+      let chosen = toprated[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeatureData(chosenInfo);
     }
